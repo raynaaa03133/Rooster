ae3e829d68b2062c04260aae0cc00274b39fbb8b
Rayna Li <raynaaa.1999@gmail.com>
2021-06-28 22:13:17 +0800

    2

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,6 +9,7 @@ class Play extends Phaser.Scene {
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
         // load spritesheet
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
         
       }
 
