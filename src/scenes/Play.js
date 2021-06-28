31fecd877e32923252a14d854b85a59fc8e481a9
Rayna Li <raynaaa.1999@gmail.com>
2021-06-28 23:51:43 +0800

    3

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -46,6 +46,13 @@ class Play extends Phaser.Scene {
       keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
       keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
       keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+      // animation config
+      this.anims.create({
+       key: 'explode',
+      frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
+      frameRate: 30
+      });
     }
     
     update() {
@@ -55,19 +62,19 @@ class Play extends Phaser.Scene {
       this.ship02.update();
       this.ship03.update();
 
-        // check collisions
+       // check collisions
       if(this.checkCollision(this.p1Rocket, this.ship03)) {
         this.p1Rocket.reset();
-        this.ship03.reset();
+        this.shipExplode(this.ship03);   
       }
       if (this.checkCollision(this.p1Rocket, this.ship02)) {
         this.p1Rocket.reset();
-        this.ship02.reset();
+        this.shipExplode(this.ship02);
       }
       if (this.checkCollision(this.p1Rocket, this.ship01)) {
         this.p1Rocket.reset();
-        this.ship01.reset();
-      }
+        this.shipExplode(this.ship01);
+      } 
     }
 
     checkCollision(rocket, ship) {
@@ -80,5 +87,18 @@ class Play extends Phaser.Scene {
       } else {
           return false;
       }
+  }
+
+  shipExplode(ship) {
+    // temporarily hide ship
+    ship.alpha = 0;
+    // create explosion sprite at ship's position
+    let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+    boom.anims.play('explode');             // play explode animation
+    boom.on('animationcomplete', () => {    // callback after anim completes
+      ship.reset();                         // reset ship position
+      ship.alpha = 1;                       // make ship visible again
+      boom.destroy();                       // remove explosion sprite
+    });       
   }
 }
