48073b61a26a03bea3e875cfe5a98e894226e0f5
Rayna Li <raynaaa.1999@gmail.com>
2021-07-01 16:53:27 +0800

    main

--- src/main.js
+++ src/main.js
@@ -11,4 +11,8 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
-//
+//Rayna Li, Rooster,6/29/2021, 8h
+//Point Breakdown:
+//20,Finishing the Tutorial +
+//60,Redesign the game's artwork, UI, and sound to change its theme/aesthetic
+//20,Create new artwork for all of the in-game assets 
